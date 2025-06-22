44ec7bcd0bb979d33083dc46ab703da3e0a265d1
devheris <badairoinioluwa578@gmail.com>
2025-06-22 02:15:08 +0100

    fix: use @tanstack/react-query for Providers and install dependency for Vercel build

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ReactNode } from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import { I18nextProvider } from "react-i18next";
